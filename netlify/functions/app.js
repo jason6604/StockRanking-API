c62eb6b04c237fa6523156fb167e3ce02d93b869
jason6604 <jasontsai6604@gmail.com>
2025-03-10 13:26:48 +0900

    Set netlify config
    Set netlify config

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -2,29 +2,27 @@ import express from 'express'
 import serverless from 'serverless-http';
 import fs from 'fs'
 
-export async function handler(event, context) {
-    const api = express();
-    const router = express.Router();
-    const PORT = process.env.PORT || 3000;
-    api.use(express.json());
+const api = express();
+const router = express.Router();
+const PORT = process.env.PORT || 3000;
+api.use(express.json());
 
-    api.listen(PORT, () => {
-        console.log(`Demo API Server is running on port ${PORT}`)
-    });
+api.listen(PORT, () => {
+    console.log(`Demo API Server is running on port ${PORT}`)
+});
 
-    const rankingStockData = JSON.parse(fs.readFileSync( "./StockRanking.json", 'utf8'));
+const rankingStockData = JSON.parse(fs.readFileSync("./StockRanking.json", 'utf8'));
 
-    router.get("/topstocks", (req, res) => {
-        res.json(rankingStockData.data);
-    })
+router.get("/topstocks", (req, res) => {
+    res.json(rankingStockData.data);
+})
 
-    router.get("/topstocksbyrank/:rank", (req, res) => {
-        const rank = parseInt(req.params.rank);
-        const outputData = rankingStockData.data.filter(stock => stock.ranking === rank);
-        res.json(outputData);
-    })
+router.get("/topstocksbyrank/:rank", (req, res) => {
+    const rank = parseInt(req.params.rank);
+    const outputData = rankingStockData.data.filter(stock => stock.ranking === rank);
+    res.json(outputData);
+})
 
-    api.use("/stockapi/", router);
+api.use("/stockapi/", router);
 
-    return serverless(api)(event, context);
-}
+export const handler = serverless(api);
