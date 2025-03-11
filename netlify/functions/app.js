dcbac555205ebc790495c17636a990e82e36b875
jason6604 <jasontsai6604@gmail.com>
2025-03-11 12:37:16 +0900

    Add Stock Data & fixed files couldn't find issue.
    Add Stock Data & fixed files couldn't find issue.

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,19 +1,11 @@
 import express from 'express'
 import serverless from 'serverless-http';
-import cors from 'cors';
 import fs from 'fs'
 
 const api = express();
 const router = express.Router();
 const PORT = process.env.PORT || 3000;
-const corsOptions = {
-    origin: ['*'],
-    methods: 'GET,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
-
 api.use(express.json());
-api.use(cors(corsOptions));
 
 api.listen(PORT, () => {
     console.log(`Demo API Server is running on port ${PORT}`)
